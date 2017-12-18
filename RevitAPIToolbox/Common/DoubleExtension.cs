40ca0e6763ec54588f3017b27fb669d0e94ccf65
KennanChan <chenkn@glodon.com>
2017-12-18 11:23:49 +0800

    add some new extensions and conditional compile symbols

---
+++ RevitAPIToolbox/Common/DoubleExtension.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.DB;
+
+namespace Techyard.Revit.Common
+{
+    internal static class DoubleExtension
+    {
+        /// <summary>
+        ///     Translate a value in millimeter to revit internal unit
+        /// </summary>
+        /// <param name="millimeter">Value in millimeter</param>
+        /// <returns>Value in feet</returns>
+        internal static double MillimeterToInternal(this double millimeter)
+        {
+            return millimeter.ToInternal(DisplayUnitType.DUT_MILLIMETERS);
+        }
+
+        /// <summary>
+        ///     Translate a value in square millimeter to revit internal unit
+        /// </summary>
+        /// <param name="squareMillimeter">Value in square millimeter</param>
+        /// <returns>Value in square feet</returns>
+        internal static double SquareMillimeterToInternal(this double squareMillimeter)
+        {
+            return squareMillimeter.ToInternal(DisplayUnitType.DUT_SQUARE_MILLIMETERS);
+        }
+
+        /// <summary>
+        ///     Translate a value in cube millimeter to revit internal unit
+        /// </summary>
+        /// <param name="cubeMillimeter">Value in cube millimeter</param>
+        /// <returns>Value in cube feet</returns>
+        internal static double CubeMillimeterToInternal(this double cubeMillimeter)
+        {
+            return cubeMillimeter.ToInternal(DisplayUnitType.DUT_CUBIC_MILLIMETERS);
+        }
+
+        /// <summary>
+        ///     Translate a value to revit internal unit
+        /// </summary>
+        /// <param name="display">Value</param>
+        /// <param name="unit">Unit</param>
+        /// <returns>Value in revit internal unit</returns>
+        internal static double ToInternal(this double display, DisplayUnitType unit)
+        {
+            return UnitUtils.ConvertToInternalUnits(display, unit);
+        }
+    }
+}
--- RevitAPIToolbox/Database/FamilyExtension.cs
+++ RevitAPIToolbox/Database/FamilyExtension.cs
@@ -1,6 +1,46 @@
-﻿namespace Techyard.Revit.Database
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Techyard.Revit.Common;
+
+namespace Techyard.Revit.Database
 {
     public static class FamilyExtension
     {
+        public static ElementId GetCategoryId(this Family family)
+        {
+            try
+            {
+                return family.FamilyCategory.Id;
+            }
+            catch
+            {
+                return GetFamilySymbols(family).FirstOrDefault()?.Category?.Id;
+            }
+        }
+
+        internal static IEnumerable<FamilySymbol> GetFamilySymbols(this Family family)
+        {
+            var document = family.Document;
+            try
+            {
+#if REVIT2014
+                return family.Symbols.AsList<FamilySymbol>();
+#elif REVIT2015 || REVIT2016 || REVIT2017 || REVIT2018
+                return
+                    family.GetFamilySymbolIds()
+                        .Map<ElementId, FamilySymbol>(symbolId => document.GetElement(symbolId) as FamilySymbol)
+                        .Where(symbol => symbol != null);
+#endif
+            }
+            catch
+            {
+                return
+                    new FilteredElementCollector(document).OfClass(typeof(FamilySymbol))
+                        .Cast<FamilySymbol>()
+                        .Where(symbol => family.Id.Equals(symbol.Family.Id));
+            }
+        }
     }
 }
